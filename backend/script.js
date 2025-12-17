d553e1364b3ded43ab80aa6492442663fbf0a4b2
IvyYang <ivy990030@icloud.com>
2025-12-17 13:22:26 +0800

    feat: add renderOderList function

--- backend/script.js
+++ backend/script.js
@@ -10,6 +10,22 @@ const dom = {
     deleteAllBtn: document.querySelector(".discardAllBtn")
 };
 
+const orderListHead = `
+    <thead>
+            <tr>
+              <th>訂單編號</th>
+              <th>聯絡人</th>
+              <th>聯絡地址</th>
+              <th>電子郵件</th>
+              <th>訂單品項</th>
+              <th>訂單日期</th>
+              <th>訂單狀態</th>
+              <th>操作</th>
+            </tr>
+          </thead>
+`;
+
+
 
 let ordersData = [];
 
@@ -25,3 +41,38 @@ async function getOrdersData() {
         console.log(error);
     }
 }
+
+
+function renderOrdersList(orders) {
+    let orderItems = orders.map(order=>{
+        let orderStatus = '';
+        console.log(order.paid)
+        if(!order.paid){
+            orderStatus = '未處理';
+        }else{
+            orderStatus = '已付款';
+        }
+        let orderItem = `<tr>
+            <td>${order.id}</td>
+            <td>
+              <p>${order.user.name}</p>
+              <p>${order.user.tel}</p>
+            </td>
+            <td>${order.user.address}</td>
+            <td>${order.user.email}</td>
+            <td>
+              <p>${order.products[0].title}</p>
+            </td>
+            <td>${new Date(order.createdAt * 1000).toLocaleDateString()}</td>
+            <td class="orderStatus">
+              <a href="#">${orderStatus}</a>
+            </td>
+            <td>
+              <input type="button" class="delSingleOrder-Btn" value="刪除" />
+            </td>
+          </tr>`
+        return orderItem;
+    }).join('');
+    dom.orderList.innerHTML = orderListHead + orderItems;
+};
+
