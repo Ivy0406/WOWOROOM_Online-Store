7607b17b7e921f5fb8a047301a206306644fc441
IvyYang <ivy990030@icloud.com>
2025-12-17 15:01:21 +0800

    feat: add 'edit order status' function

--- backend/script.js
+++ backend/script.js
@@ -1,5 +1,5 @@
 const Orders_Api_url = "https://livejs-api.hexschool.io/api/livejs/v1/admin/ivy1215/orders";
-const adminToken = {
+const Admin_Token = {
       headers: {
         'Authorization': 'goO12Xq0pVMaW3EOLuZbrHhjXxG3'
       }
@@ -33,7 +33,7 @@ getOrdersData();
 
 async function getOrdersData() {
     try {
-        let res = await axios.get(Orders_Api_url,adminToken);
+        let res = await axios.get(Orders_Api_url,Admin_Token);
         ordersData = res.data.orders;
         console.log(ordersData);
         renderOrdersList(ordersData);
@@ -46,14 +46,13 @@ async function getOrdersData() {
 function renderOrdersList(orders) {
     let orderItems = orders.map(order=>{
         let orderStatus = '';
-        console.log(order.paid)
         if(!order.paid){
             orderStatus = '未處理';
         }else{
             orderStatus = '已付款';
         }
-        let orderItem = `<tr>
-            <td>${order.id}</td>
+        let orderItem = `<tr class="order-item">
+            <td class="order-id">${order.id}</td>
             <td>
               <p>${order.user.name}</p>
               <p>${order.user.tel}</p>
@@ -74,5 +73,48 @@ function renderOrdersList(orders) {
         return orderItem;
     }).join('');
     dom.orderList.innerHTML = orderListHead + orderItems;
+
+    const orderStatusBtns = document.querySelectorAll(".orderStatus");
+    orderStatusBtns.forEach(btn=>{
+        btn.addEventListener('click',function(e){
+            e.preventDefault();
+            let targetOrder = e.target.closest(".order-item");
+            console.log(targetOrder);
+            editOrderStatus(targetOrder);
+        })
+    })
+    
 };
 
+function editOrderStatus(targetOrder){
+    let result = confirm("您是否要修改訂單狀態呢？");
+    if(!result){
+        return
+    }else{
+        let targetId = targetOrder.querySelector(".order-id").textContent.trim();
+        let targetStatus = targetOrder.querySelector(".orderStatus").textContent.trim();
+        let isPaid = targetStatus === "已付款";
+        let nextStatus = !isPaid;
+        putOrders(targetId,nextStatus);
+        
+            
+    }
+        
+    
+}
+
+async function putOrders(id,status) {
+    const targetDataToPut = {
+            data: {
+            id: id,
+            paid: status
+            }
+        }
+    try {
+        let res = await axios.put(Orders_Api_url,targetDataToPut,Admin_Token)
+        ordersData = res.data.orders;
+        renderOrdersList(ordersData);
+    } catch (error) {
+        console.log(error);
+    }
+}
